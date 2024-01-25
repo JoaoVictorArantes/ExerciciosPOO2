69d082fc6eef49e90c164d83339289292bb94c97
João Victor <jvaarantes@gmail.com>
2024-01-25 09:17:42 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPOO2/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO2
+{
+    internal class Funcionario
+    {
+        public String Nome;
+        public double Salario;
+
+        public static double MediaSalario(double Salario1, double Salario2)
+        {
+            return ((Salario1 + Salario2) / 2);
+        }
+    }
+}
---
+++ ExerciciosPOO2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExerciciosPOO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+
+            Console.Write("Salario: ");
+            funcionario1.Salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+
+            Console.Write("Salario: ");
+            funcionario2.Salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"O salário médio é: { Funcionario.MediaSalario(funcionario1.Salario, funcionario2.Salario)}");
+
+            Console.ReadKey();
+        }
+    }
+}
